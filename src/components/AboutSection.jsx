15128f2d4a15a20a5451610bea2256b7bf20ce54
Mahmoud <mazenmahmod201@gmail.com>
2025-06-22 17:26:08 +0300

    Minor content changes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -15,7 +15,7 @@ export default function AboutSection() {
                     </h3>
                     <div className="w-full h-px mb-4 bg-gradient-to-r from-transparent via-primary/30 to-transparent"></div>
                     <p className="text-muted-foreground text-md md:text-lg lg:text-lg mb-4 max-w-3xl mx-auto">
-                        I’m a recent Software Engineering graduate from Jordan University of Science and Technology with strong experience in full-stack web development, user interface design, and problem-solving. I’ve built user-focused applications using the MERN stack, and completed hands-on training through many projects and courses.
+                        I’m a recent Software Engineering graduate from Jordan University of Science and Technology with strong experience in full-stack web development, user interface design, and problem-solving. I’ve built user-focused applications using the MERN stack, and continuously expand my skill set through practical projects and online training.
                     </p>
                     <p className="text-muted-foreground text-md md:text-lg lg:text-lg mb-4 max-w-3xl mx-auto">
                         I've over 5 years of customer-facing experience, bringing excellent communication, tech support, and multitasking skills to every team I join. Fluent in Arabic and English and passionate about clean code, thoughtful UX, and continuous learning. 
@@ -66,7 +66,7 @@ export default function AboutSection() {
                             <div className="text-left">
                                 <h4 className="font-semibold text-lg mb-2">Technical Expertise</h4>
                                 <p className="text-muted-foreground mb-3">
-                                    Experienced with the MERN stack, building responsive frontend interfaces and functional backend systems.
+                                    Experienced in React, Node.js, and MongoDB, with a continuous interest in learning new technologies and best practices in web development.
                                 </p>
                             </div>
                         </div>
